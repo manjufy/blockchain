4f25cbcdc6689cb8f8810d0aa3e9212fb40a81fe
Manjunath Reddy <manju16832003@gmail.com>
2018-10-12 23:55:38 +0800

    New chain

--- manju-coin/main.js
+++ manju-coin/main.js
@@ -34,10 +34,30 @@ class Blockchain {
         // in reality, this requires lot more work to add a new block
         this.chain.push(newBlock)
     }
+
+    // check the integrity of the block
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i]
+            const previousBlock = this.chain[i - 1]
+
+            if (currentBlock.hash != currentBlock.calculateHash()) {
+                return false
+            }
+
+            // check if current block hash = previous block
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false
+            }
+
+            return true
+        }
+    }
 }
 
 let manjuCoin = new Blockchain();
 manjuCoin.addBlock(new Block(1, '01-02-2018', { cash: 10 }))
 manjuCoin.addBlock(new Block(1, '01-03-2018', { cash: 12 }))
 
-console.log('Manju Coin ', JSON.stringify(manjuCoin, null, 4))
+// console.log('Manju Coin ', JSON.stringify(manjuCoin, null, 4))
+console.log('Is chain valid', manjuCoin.isChainValid())
